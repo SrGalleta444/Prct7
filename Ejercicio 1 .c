43bd0edf74fc6f26a47960d9a2553b306ca8243e
SrGalleta444 <cnm.arielanaya@gmail.com>
2025-03-27 14:02:46 -0600

    Create Ejercicio 1 .c

---
+++ Ejercicio 1 .c
@@ -0,0 +1,21 @@
+/*Practica 7 
+Ariel Anaya ejercicio 1 
+Que numero es mayor*/
+
+#include <stdio.h>
+
+int main()
+{
+   int a, b;
+   printf("introduce un numero");
+   scanf("%d", &a);
+   printf("introduce oro numero");
+   scanf("%d", &b);
+   
+   if( a < b){
+       printf("a es menor que b");
+      
+   }
+printf("fin del programa");
+    
+}
