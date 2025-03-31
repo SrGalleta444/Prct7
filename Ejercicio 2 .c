4114baf7821885c2a0dd777b9e2ab2f241241c83
SrGalleta444 <cnm.arielanaya@gmail.com>
2025-03-30 19:29:11 -0600

    Update and rename 2 .c to Ejercicio 2 .c

---
+++ Ejercicio 2 .c
@@ -0,0 +1,23 @@
+// Ariel Anaya Practica 7 
+//Ejercicio 2 
+
+
+#include <stdio.h>
+void main()
+{
+    int n;
+    printf("Introduce un numero");
+    scanf("%i", &n);
+    
+    if(n % 2 == 0)
+    {
+        printf("el numero es par");
+
+    }
+    else(n % 2 != 0);
+    {
+    printf("el numero es impar");
+    }
+    
+
+}
