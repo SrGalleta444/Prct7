1afda8ae87084eafc83e9da6b41bdec7f7d24051
SrGalleta444 <cnm.arielanaya@gmail.com>
2025-03-30 19:49:44 -0600

    Update Calculadora .c

--- Calc .c
+++ Calc .c
@@ -11,6 +11,7 @@ void main()
     int op;
     
     printf("Menu: \n1: suma \n2: resta \n3: multiplicacion \n4: division \n5: potenciacion \n 6: radicación \n0: fin");
+    printf("Elige una opcion");
     scanf("%d", &op);
     
     if(op != 0){
@@ -20,9 +21,46 @@ void main()
         scanf("%d", &b);
     }
     
+    
+    
     switch(op){
-        case 1;
+        
+        case 1:
         res = a + b;
         printf("%d", res);
+        break;
+        
+        case 2:
+        res = a - b;
+        printf("%d", res);
+        break;
+        
+        case 3:
+        res = a * b;
+        printf("%d", res);
+        break;
+        
+        case 4:
+        res = a / b;
+        printf("%d", res);
+        break;
+        
+        case 5:
+        res = pow(a, b);
+        printf("%d", res);
+        break;
+        
+        
     }
+    
+    if(op==6){
+        printf("ingrese un numero");
+        scanf("%d", &a);
+        switch(op){
+            case 6:
+            res = sqrt(a);
+            printf("%d", res);
+            break;
+        }
     }
+}
